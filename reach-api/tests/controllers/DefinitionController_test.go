7ca76bbf52fb720c8d09af0a73942650f784bb40
Stefan Ciprian Hotoleanu <hotoleanu.ciprian@gmail.com>
2022-11-12 15:06:52 +0200

    Generated tests for controllers

---
+++ reach-api/tests/controllers/DefinitionController_test.go
@@ -0,0 +1,93 @@
+package tests
+
+import (
+	"reach/reach-api/controllers"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestGetDefinition(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.GetDefinition(tt.args.c)
+		})
+	}
+}
+
+func TestCreateDefinition(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.CreateDefinition(tt.args.c)
+		})
+	}
+}
+
+func TestGetDefinitionByID(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.GetDefinitionByID(tt.args.c)
+		})
+	}
+}
+
+func TestUpdateDefinition(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.UpdateDefinition(tt.args.c)
+		})
+	}
+}
+
+func TestDeleteDefinition(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.DeleteDefinition(tt.args.c)
+		})
+	}
+}
---
+++ reach-api/tests/controllers/TransformerController_test.go
@@ -0,0 +1,93 @@
+package tests
+
+import (
+	"reach/reach-api/controllers"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestGetTransformer(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.GetTransformer(tt.args.c)
+		})
+	}
+}
+
+func TestCreateTransformer(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.CreateTransformer(tt.args.c)
+		})
+	}
+}
+
+func TestGetTransformerByID(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.GetTransformerByID(tt.args.c)
+		})
+	}
+}
+
+func TestUpdateTransformer(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.UpdateTransformer(tt.args.c)
+		})
+	}
+}
+
+func TestDeleteTransformer(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllers.DeleteTransformer(tt.args.c)
+		})
+	}
+}
